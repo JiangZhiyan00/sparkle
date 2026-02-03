f137c366d049f75c5b7e4bc69e6df88e8e21c35c
jiangzhiyan <zhiyanjiang@gearsnet.com>
2026-02-03 13:15:35 +0800

    refactor: 移除冗余的 versionTag 变量，统一使用 latest.version 处理版本信息。

--- scripts/updater.mjs
+++ scripts/updater.mjs
@@ -4,18 +4,17 @@ import { readFileSync, writeFileSync } from 'fs'
 const pkg = readFileSync('package.json', 'utf-8')
 let changelog = readFileSync('changelog.md', 'utf-8')
 const { version } = JSON.parse(pkg)
-const versionTag = version.startsWith('v') ? version : `v${version}`
-const downloadUrl = `https://github.com/jiangzhiyan00/sparkle/releases/download/${versionTag}`
 const latest = {
-  versionTag,
+  version: version.startsWith('v') ? version : `v${version}`,
   changelog
 }
+const downloadUrl = `https://github.com/jiangzhiyan00/sparkle/releases/download/${latest.version}`
 
 if (process.env.SKIP_CHANGELOG !== '1') {
   changelog += '\n### 下载地址：\n\n#### Windows10/11：\n\n'
-  changelog += `- 安装版：[64位](${downloadUrl}/sparkle-windows-${versionTag}-x64-setup.exe)\n\n`
+  changelog += `- 安装版：[64位](${downloadUrl}/sparkle-windows-${latest.version}-x64-setup.exe)\n\n`
   changelog += '\n#### macOS 11+：\n\n'
-  changelog += `- PKG：[Apple Silicon](${downloadUrl}/sparkle-macos-${versionTag}-arm64.pkg)\n\n`
+  changelog += `- PKG：[Apple Silicon](${downloadUrl}/sparkle-macos-${latest.version}-arm64.pkg)\n\n`
   changelog += '\n#### Linux：\n\n'
 }
 writeFileSync('latest.yml', yaml.stringify(latest))
